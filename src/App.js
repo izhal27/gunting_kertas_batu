674d1e4684c0ee9288b2affa573cbd17377f7f73
Risal Walangadi <risal.walangadi@gmail.com>
2019-11-26 14:59:35 +0800

    added images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
+const scissorsRight = require('./images/scissors-right.png');
+const paperRight = require('./images/paper-right.png');
+const rockRight = require('./images/rock-right.png');
+
+const imagesRight = [scissorsRight, paperRight, rockRight];
+
+const scissorsLeft = require('./images/scissors-left.png');
+const paperLeft = require('./images/paper-left.png');
+const rockLeft = require('./images/rock-left.png');
+
+const imagesLeft = [scissorsLeft, paperLeft, rockLeft];
+
+function Image(props) {
   return (
-    <>
-      <h1>Hello ReactJS</h1>
-    </>
-  );
+    <img src={props.img} alt='image' />
+  )
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      indexRight: 0,
+      indexLeft: 0,
+    }
+  }
+
+  generateImage() {
+    const idGen = setInterval(() => {
+      this.setState({
+        indexRight: Math.floor(Math.random() * 3),
+        indexLeft: Math.floor(Math.random() * 3),
+      });
+    }, 75)
+
+    setTimeout(() => {
+      clearInterval(idGen)
+    }, 2000);
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Gunting Kertas Batu</h1>
+        <Image img={imagesRight[this.state.indexRight]} /><br />
+        <Image img={imagesLeft[this.state.indexLeft]} /><br />
+        <button onClick={() => this.generateImage()}>Random image</button>
+      </>
+    );
+  }
 }
 
 export default App;

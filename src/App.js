208b3e824911288f46b45d4029f29a12bbdb48a0
Risal Walangadi <risal.walangadi@gmail.com>
2019-11-27 14:43:53 +0800

    add calculate winner

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function CenterContainer(props) {
         {props.children}
       </Col>
     </Row>
-  )
+  );
 }
 
 function CenterRow(props) {
@@ -35,7 +35,33 @@ function CenterRow(props) {
     <Row className='justify-content-md-center'>
       {props.children}
     </Row>
-  )
+  );
+}
+
+function Header() {
+  return (
+    <CenterContainer>
+      <h1>Gunting Kertas Batu</h1>
+    </CenterContainer>
+  );
+}
+
+function Status(props) {
+  return (
+    <CenterRow>
+      <Col md='auto'>
+        <h3>You</h3>
+        <h3 className='d-flex justify-content-center'>{props.humanScore}</h3>
+      </Col>
+      <Col md='auto'>
+        <h3>VS</h3>
+      </Col>
+      <Col md='auto'>
+        <h3>Comp</h3>
+        <h3 className='d-flex justify-content-center'>{props.compScore}</h3>
+      </Col>
+    </CenterRow>
+  );
 }
 
 class App extends Component {
@@ -45,69 +71,83 @@ class App extends Component {
       indexRight: 0,
       indexLeft: 0,
       loading: false,
+      winner: '',
+      humanScore: 0,
+      compScore: 0,
       show: false,
     };
   }
 
-  generateImage() {
+  calculateWinner() {
+    var result = (3 + this.state.indexLeft - this.state.indexRight) % 3;
+    if (!result) {
+      return 'Draw';
+    } else if (result === 1) {
+      this.setState(state => ({ humanScore: ++state.humanScore }));
+      return 'You Win';
+    } else {
+      this.setState(state => ({ compScore: ++state.compScore }));
+      return 'Comp Win';
+    }
+  }
+
+  randomImage() {
     this.setState({ loading: true });
-    const idGen = setInterval(() => {
+    const randomId = setInterval(() => {
       this.setState({
-        indexRight: Math.floor(Math.random() * 3),
         indexLeft: Math.floor(Math.random() * 3),
+        indexRight: Math.floor(Math.random() * 3),
       });
     }, 75);
     setTimeout(() => {
-      clearInterval(idGen);
-      this.setState({
-        loading: false,
-        show: true,
-      });
+      clearInterval(randomId);
+      setTimeout(() => {
+        this.setState({
+          winner: this.calculateWinner(),
+          show: true,
+        });
+      }, 1000);
     }, 2000);
   }
 
   render() {
     const btnTxt = this.state.loading ? 'Loading...' : 'Start';
+    const humanScore = this.state.humanScore;
+    const compScore = this.state.compScore;
+    const youImage = imagesLeft[this.state.indexLeft];
+    const compImage = imagesRight[this.state.indexRight];
+    const loading = this.state.loading;
+    const show = this.state.show;
+    const winner = this.state.winner;
     return (
       <>
         <Container className='mt-lg-5'>
-          <CenterContainer>
-            <h1>Gunting Kertas Batu</h1>
-          </CenterContainer>
+          <Header />
+          <Status humanScore={humanScore} compScore={compScore} />
           <CenterRow>
             <Col md='auto'>
-              <h3>You</h3>
-              <h3 className='d-flex justify-content-center'>0</h3>
+              <Image img={compImage} />
             </Col>
             <Col md='auto'>
-              <h3>VS</h3>
-            </Col>
-            <Col md='auto'>
-              <h3>Comp</h3>
-              <h3 className='d-flex justify-content-center'>0</h3>
-            </Col>
-          </CenterRow>
-          <CenterRow>
-            <Col md='auto'>
-              <Image img={imagesRight[this.state.indexRight]} />
-            </Col>
-            <Col md='auto'>
-              <Image img={imagesLeft[this.state.indexLeft]} />
+              <Image img={youImage} />
             </Col>
           </CenterRow>
           <CenterContainer>
             <Button
-              onClick={() => this.generateImage()}
-              disabled={this.state.loading}>
+              onClick={() => this.randomImage()}
+              disabled={loading}>
               {btnTxt}
             </Button>
           </CenterContainer>
           <SweetAlert
-            show={this.state.show}
-            type='success'
+            show={show}
+            type='info'
             title="Result"
-            text="You Win"
-            onConfirm={() => this.setState({ show: false })}
+            text={winner}
+            onConfirm={() => this.setState({
+              show: false,
+              loading: false,
+            })}
           />
         </Container>
       </>
